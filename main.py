ded5d78d72800d779f4de8756be9f1e312e835a8
Anirudh1731 <anirudhsanthosh1729@gmail.com>
2024-12-07 18:56:15 +0530

    Data Transformation added

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.artifact_entity import DataIngestionArtifact
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
@@ -20,14 +21,26 @@ if __name__=='__main__':
         logging.info(f"Exported train and test file path.")
 
         data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+        print(data_ingestion_artifact)
+
         logging.info("Data Ingestion is completed")
-        logging.info("Data Validataion is completed")
+        logging.info("Data Validataion initiated")
 
         data_validation_config=DataValidationConfig(training_pipeline_config)
         data_validation=DataValidation(data_ingestion_artifact,data_validation_config)
         data_validation_artifact=data_validation.initiate_data_validation()
-
         print(data_validation_artifact)
-        print(data_ingestion_artifact)
+        logging.info("Data Validataion is completed")
+        logging.info("Data Transformation initiated")
+
+        data_transformation_config=DataTransformationConfig(training_pipeline_config)
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("Data Transformation completed")
+
+
+
+
     except Exception as e:
         raise NetworkSecurityException(e,sys)
